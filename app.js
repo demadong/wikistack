08b60c7bca8d339aa72167670953e34637dea016
Matthew Kim <matthewkim93@gmail.com>
2015-11-16 17:25:39 -0500

    set up app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express'),
+	morgan = require('morgan'),
+	bodyParser = require('body-parser'),
+	swig = require('swig')
+
+var PORT = 1337,
+	app = express(),
+	routes = require('./routes/');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+swig.setDefaults({cache: false});
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+// dynamic routing
+app.use('/', routes);
+
+// static routing
+app.use(express.static(__dirname + '/public'));
+
+//error handling middleware
+app.use(function(req, res, next){
+  var err = new Error('could not find route');
+  err.status = 404;
+  next(err); // passing a truthy value to `next` goes to error middleware
+});
+
+// a custom error-handling middleware function
+app.use(function(err, req, res, next){ // 4 params -> error-handling middleware
+  res.status(err.status || 500).send('ERROR: ' + err.message);
+});
+
+app.listen(PORT, function() {
+	console.log('Listening to port', PORT);
+});
