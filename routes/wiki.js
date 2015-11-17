588a163ef4d43ac98d6cda4214bbad44a8a3a4c9
Matthew Kim <matthewkim93@gmail.com>
2015-11-17 11:37:26 -0500

    Completed up to displaying single wiki page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,16 +1,38 @@
 var express = require('express'),
-	router = express.Router();
+	router = express.Router(),
+	models = require('../models/'),
+	Page = models.Page,
+	User = models.User;
 
 router.get('/', function(req, res, next) {
   res.redirect('/');
 });
 
 router.post('/', function(req, res, next) {
-  res.json(req.body);
+  var page = new Page({
+    title: req.body.title,
+    content: req.body.content
+    // status: req.body.status,
+  });
+  console.log(page.title, page.content);
+
+  // STUDENT ASSIGNMENT:
+  // make sure we only redirect *after* our save is complete!
+  // note: `.save` returns a promise or it can take a callback.
+  page.save().then(function(savedPage){
+  	res.redirect(savedPage.urlTitle);
+  }).then(null, next);
+  // -> after save -> res.redirect('/');
 });
 
 router.get('/add', function(req, res, next) {
   res.render('addpage');
+});
+
+router.get('/:urlTitle', function(req, res, next) {
+	Page.findOne({urlTitle: req.params.urlTitle}).exec().then(function(page){
+		res.render('wikipage', {page:page});
+	})
 });
 
 module.exports = router;
